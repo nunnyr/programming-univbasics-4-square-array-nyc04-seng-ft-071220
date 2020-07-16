1ad3c2a6da0867cb810332e47fdb6ea4b844564d
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-16 21:38:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,27 @@
+
+
+
+
 def square_array(array)
-  # your code here
+  count = 0
+  final = []
+  
+  
+  while count < array.length do
+  
+  
+  p "this is counter #{count }"
+  p "this is the element #{array[count]}"
+  p "this is numbers exponent #{array[count] ** array[count]}"
+  
+  
+  
+  result = (array[count] ** array[count])
+  final.push(result)
+  count += 1 
+  
+  
+  end
+  
+ return final
 end
