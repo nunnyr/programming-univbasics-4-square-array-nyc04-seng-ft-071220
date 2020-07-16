e32e66bca7ee0a6d3207af27264565e2af635db2
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-16 22:04:07 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,20 @@
 def square_array(array)
-  # your code here
+  count = 0
+  final = []
+  
+  
+  while count < array.length
+  
+  p "this is counter #{count }"
+  p "this is the element #{array[count]}"
+ 
+  
+  result = (array[count] ** 2)
+  final.push(result)
+  count += 1 
+  
+  
+  end
+  p final
 end
+
